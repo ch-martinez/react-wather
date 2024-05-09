c19eb0afca89362621b5c7b5949589b3ddce5cc1
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-08 23:04:21 -0300

    feat: spinner

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,26 @@
 import { Location } from "./Location"
 import { Current } from "./Current"
 import { Forecast } from "./Forecast"
+import { Spinner } from "./Spinner"
 
-export const Weather = ({weatherData}) => {
-    const location = weatherData.location
-    const current = weatherData.current
-    const forecast = weatherData.forecast.forecastday
+export const Weather = ({isLoading, showData, weatherData}) => {
 
-    return(
-        <>
-            <Location location={location}/>
-            <Current current={current}/>
-            <Forecast forecast={forecast}/>
-        </>
-    )
+    if (isLoading) {return <Spinner/>}
+
+    if(showData == true){
+        const location = weatherData.location
+        const current = weatherData.current
+        const forecast = weatherData.forecast.forecastday
+        return(
+            <>
+                <Location location={location}/>
+                <Current current={current}/>
+                <Forecast forecast={forecast}/>
+            </>
+        )
+    }else{
+        return(
+            <></>
+        )
+    }
 }
