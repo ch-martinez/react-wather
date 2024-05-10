f797c622fc766714c52a757951b33bf256ee8911
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-09 23:19:22 -0300

    feat: se añade lectura de ciudad por medio de params

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { SearchBar } from "../components/SearchBar"
 import { Weather } from "../components/Weather"
 import { getWeatherDataFromAPI } from "../utils/weatherAPI"
+import { useParams } from "react-router-dom"
 
 export const MainPage = () => {
     const [loading, setLoading] = useState(false)
     const [show, setShow] = useState(false)
     const [weatherData, setWeatherData] = useState(null)
+
+    let params = useParams()
 
     const handleSearch = async (input) => {
         setLoading(true)
@@ -22,6 +25,14 @@ export const MainPage = () => {
                 setShow(false)
             })
     }
+
+    //Si en la URL hay params de una ciudad, se carga y se limpia la URL
+    useEffect( ()=> {
+        if (params.city) {
+            window.history.pushState({}, document.title, "/")
+            handleSearch(params.city)
+        }
+    },[])
 
     return(
         <>
