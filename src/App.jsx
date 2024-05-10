5a6438e0b68a8d7ece09ba9f81b7d7b5979a3063
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-09 23:19:45 -0300

    feat: se añade manejo de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Navbar } from "./components/Navbar"
+import { FavoritesPage } from "./pages/FavoritesPage"
 import { MainPage } from "./pages/MainPage"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
 
 export const App = () => {
     return(
-        <>
-            <MainPage/>
-        </>
+        <BrowserRouter>
+            <header>
+                <Navbar/>
+            </header>
+            <body>
+                <Routes>
+                    <Route path="/" element={<MainPage/>} />
+                    <Route path="/:city" element={<MainPage/>} />
+                    <Route path="/favorites" element={<FavoritesPage/>} />
+                </Routes>
+            </body>
+        </BrowserRouter>
     )
 }
