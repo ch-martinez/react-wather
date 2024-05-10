986043eece25946dd30e0c918a3baeab0f1c375a
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-09 23:18:22 -0300

    feat: funciones para manipular datos en firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,18 +1,36 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { collection, doc, getDocs, deleteDoc, setDoc, addDoc } from "firebase/firestore"
+import { db } from "./firebaseConfig"
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyATFhtOXVj1nJasEAwpwClcGFJJICWN9_g",
-  authDomain: "ch-react-weather.firebaseapp.com",
-  projectId: "ch-react-weather",
-  storageBucket: "ch-react-weather.appspot.com",
-  messagingSenderId: "229653224687",
-  appId: "1:229653224687:web:32f8797ddfea94c22c4f1a"
-};
+//Referenciamos a la base de datos (favorites) de firestore
+const favoritesCollections = collection(db,'favorites')
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//Obtenemos todos los documentos 'favorites'
+export const getFavorites = async () => {
+    const res = await getDocs(favoritesCollections)
+    return res.docs.map ((doc) => (
+        {
+            ...doc.data(),
+            id:doc.id
+        }
+    ))
+}
 
-export const db = getFirestore(app)
+export const addFavorite = async (name) => {
+    const favorite = doc(favoritesCollections)
+    await addDoc(favoritesCollections,{
+        name: name
+    })
+}
+
+export const setFavorite = async (name) => {
+    const favorite = doc(favoritesCollections)
+    await setDoc(favoritesCollections,{
+        name: name
+    })
+}
+
+// Elimina el documento indicado
+export const deletFavorite = async (id) => {
+    const favorite = doc(favoritesCollections,id)
+    await deleteDoc(favorite)
+}
