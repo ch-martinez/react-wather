5dfacd87cac693133dfacdc536f80e43e505e123
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-30 06:54:53 -0300

    fix: se optimiza codigo

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,27 +1,23 @@
-import { Location } from "./Location"
-import { Current } from "./Current"
-import { Forecast } from "./Forecast"
-import { Spinner } from "../Spinner"
+import { Current } from "./Current";
+import { Forecast } from "./Forecast";
+import { Spinner } from "../Spinner";
 
-export const Weather = ({isLoading, showData, weatherData}) => {
+export const Weather = ({ isLoading, showData, data }) => {
 
-    if (isLoading) {return <Spinner/>}
-
-    if(showData == true){
-        const location = weatherData.location
-        const current = weatherData.current
-        const forecast = weatherData.forecast.forecastday
-
-        return(
-            <>
-                <Location location={location}/>
-                <Current current={current}/>
-                <Forecast forecast={forecast}/>
-            </>
-        )
-    }else{
-        return(
-            <></>
-        )
+    if (isLoading) {
+        return <Spinner />;
     }
-}
+
+    if (showData) {
+        console.log(data)
+        return (
+            <>
+                <Current location={data.location} current={data.current} />
+                <Forecast title="Pronostico por horas" forecast={data.forecast_hours} />
+                <Forecast title="Pronostico por dias" forecast={data.forecast_days} />
+            </>
+        );
+    } else {
+        return <></>;
+    }
+};
