8d4068e0804aae17209e29b7d5925bf71ec67a92
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-29 22:31:20 -0300

    feat: ciudad segun lat y lon

--- src/utils/geoapifyAPI.js
+++ src/utils/geoapifyAPI.js
@@ -1,22 +1,10 @@
-import axios from "axios"
-
 //API URL: https://www.geoapify.com/
+import axios from "axios"
+import { formatLocationData } from "./formatData"
 
 const api_key = "0aba5f30249c4c53b1f877501e0e4b01"
 
-//Formato de datos
-const formatData = (data) => {
-    return {
-        id: data.plus_code,
-        lat: data.lat,
-        lon: data.lon,
-        city: data.city,
-        state: data.state,
-        country: data.country,
-        formatted_address: `${data.city}, ${data.state ? data.state+"," : ""} ${data.country}`
-    }
-}
-
+// Devuelve el listado de ciudades segun el nombre indicado
 export const getCitiessAPI = async (query) => {
     const res = await axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&lang=es&format=json&type=city&apiKey=${api_key}`)
     const results = res.data.results
@@ -25,8 +13,14 @@ export const getCitiessAPI = async (query) => {
     //Filtra los resultados que se repiten
     results.forEach((result) => {
         const exist = resultsAux.some((ra) => ra.id === result.plus_code)
-        if (!exist) {resultsAux.push(formatData(result))}
+        if (!exist) { resultsAux.push(formatLocationData(result)) }
     })
 
     return resultsAux
+}
+
+// Devuelve la ciudad segun la latitud y longitud indicada
+export const getReverseCityAPI = async (query) => {
+    const res = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?apiKey=${api_key}&lang=es&format=json&lat=${query.latitude}&lon=${query.longitude}&type=city`)
+    return formatLocationData(res.data.results[0])
 }
